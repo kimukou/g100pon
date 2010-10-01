0209ba5067779babd5037c0c8ef5a5340cc6c833
kimukou_26 <kimukou.buzz@gmail.com>
2010-10-01 11:49:21 +0900

    edit

--- jritest.groovy
+++ jritest.groovy
@@ -1,9 +1,9 @@
 import org.rosuda.JRI.REXP
 import org.rosuda.JRI.Rengine
 
-Rengine engine = new Rengine(["--no-save"] as String[] , false, null)
+def engine = new Rengine(["--no-save"] as String[] , false, null)
 engine.assign("a", [36] as int[] )
-REXP result = engine.eval("sqrt(a)")
-println result.asDouble()
+def result = engine.eval("sqrt(a)")
+println "result=${result.asDouble()}"
 engine.end()
 
--- run_jritest.bat
+++ run_jritest.bat
@@ -1,15 +1,15 @@
 ::  nessesary R install
 
-set JAVA_HOME=C:\opt\jdk
+set JAVA_HOME=C:/opt/jdk
 ::set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
 
-set R_HOME=C:\opt\R-2.4.1
-set PATH=%R_HOME%\bin
-set R_DOC_DIR=%R_HOME%\doc
-set R_INCLUDE_DIR=%R_HOME%\include
-set R_SHARE_DIR=%R_HOME%\share
+set R_HOME=C:/opt/R-2.4.1
+set PATH=%R_HOME%/bin
+set R_DOC_DIR=%R_HOME%/doc
+set R_INCLUDE_DIR=%R_HOME%/include
+set R_SHARE_DIR=%R_HOME%/share
 
-set RJAVA_HOME=%R_HOME%\library\rJava\jri
+set RJAVA_HOME=%R_HOME%/library/rJava\jri
 set JAVA_OPTS=-Djava.library.path=%RJAVA_HOME%
 
 
