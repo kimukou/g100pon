f7ee302d6b58ff17003412ed4939d9fddc997435
kimukou_26 <kimukou.buzz@gmail.com>
2010-10-29 19:31:26 +0900

    local

--- run_steeltest_full.bat
+++ run_steeltest_full.bat
@@ -2,6 +2,6 @@ set JAVA_HOME=C:\opt\jdk
 
 ::set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
 
-%JAVA_HOME%/bin/java %JAVA_OPTS% -cp ".;./lib/*" groovy.ui.GroovyMain steeltest_full.groovy
+%JAVA_HOME%/bin/java %JAVA_OPTS% -cp ".;./lib/*;./libsteel/*" groovy.ui.GroovyMain steeltest_full.groovy
 
 ::pause
--- run_steeltest_full.sh
+++ run_steeltest_full.sh
@@ -2,5 +2,5 @@ JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
 JAVA_OPTS='-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8'
 
 
-$JAVA_HOME/bin/java $JAVA_OPTS -cp ".:./lib/*" groovy.ui.GroovyMain steeltest_full.groovy
+$JAVA_HOME/bin/java $JAVA_OPTS -cp ".:./lib/*:./libsteel/*" groovy.ui.GroovyMain steeltest_full.groovy
 
