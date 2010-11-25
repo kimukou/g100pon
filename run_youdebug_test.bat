ce1599efb3136917ecc57137ff9f7e404b07dc08
kimukou_26 <kimukou.buzz@gmail.com>
2010-11-25 22:29:06 +0900

    Signed-off-by: kimukou_26 <kimukou.buzz@gmail.com>

--- run_youdebug_test.bat
+++ run_youdebug_test.bat
@@ -1,8 +1,7 @@
 set JAVA_HOME=C:\opt\jdk
 
 ::call run_youdebug_test_target.bat
-%JAVA_HOME%/bin/java -cp ".;./lib/*;./libyoudebug/*" org.kohsuke.youdebug.YouDebug -socket 5005 youdebug_test.ydb
+%JAVA_HOME%/bin/java -cp ".;./lib/*;./libyoudebug/*" org.kohsuke.youdebug.YouDebug -debug -debug -socket 5005 youdebug_test.ydb
 
-::%JAVA_HOME%/bin/java -jar libyoudebug/youdebug-1.2-jar-with-dependencies.jar -socket 5005 youdebug_test.ydb
-pause
+::%JAVA_HOME%/bin/java -jar libyoudebug/youdebug-1.2-jar-with-dependencies.jar socket 5005 youdebug_test.ydb
 
