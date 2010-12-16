82a7f8c1adb22e279ad6b58b0e986669cb9035c7
kimukou_26 <kimukou.buzz@gmail.com>
2010-12-16 21:00:17 +0900

    update groovy-all

--- jritest.groovy
+++
@@ -1,9 +0,0 @@
-import org.rosuda.JRI.REXP
-import org.rosuda.JRI.Rengine
-
-def engine = new Rengine(["--no-save"] as String[] , false, null)
-engine.assign("a", [36] as int[] )
-def result = engine.eval("sqrt(a)")
-println "result=${result.asDouble()}"
-engine.end()
-
