4524cea2064a7a4d456339f2509995121d2e8a8c
kimukou_26 <kimukou.buzz@gmail.com>
2011-02-16 14:50:39 +0900

    jxls-reader test

---
+++ run_jxls-reader-test.sh
@@ -0,0 +1,6 @@
+JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+JAVA_OPTS='-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8'
+
+
+$JAVA_HOME/bin/java $JAVA_OPTS -cp ".:./lib/*:./libsql/*" groovy.ui.GroovyMain jxls-reader-test.groovy
+
