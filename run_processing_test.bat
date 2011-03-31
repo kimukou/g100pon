ea00e07e799830525fad978ee38696c8190c4deb
kimukou_26 <kimukou.buzz@gmail.com>
2011-03-31 12:10:21 +0900

    testing groovyConsole

--- run_processing_test.bat
+++ run_processing_test.bat
@@ -1,6 +1,9 @@
 set JAVA_HOME=C:/opt/jdk
 ::set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
 
-%JAVA_HOME%/bin/java %JAVA_OPTS% -cp ".;./lib/*;./libprocess/*" groovy.ui.GroovyMain processing_test.groovy
+::%JAVA_HOME%/bin/java %JAVA_OPTS% -cp ".;./lib/*;./libprocess/*" groovy.ui.GroovyMain processing_test.groovy
+
+::use groovyConsole
+%JAVA_HOME%/bin/java %JAVA_OPTS% -cp ".;./lib/*;./libprocess/*" groovy.ui.Console processing_test.groovy
 
 pause
